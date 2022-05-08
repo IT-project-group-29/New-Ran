9648dfdaf105768d615b41d9540e8cd31641fc00
junting1103244407 <chejy205@mymail.unisa.edu.au>
2022-05-08 18:42:34 +0800

    update the function of sorting   by other information, and do changes on the student manegement interface.

--- WebApplication4/Controllers/DetailedInformation.cs
+++ WebApplication4/Controllers/DetailedInformation.cs
@@ -16,122 +16,181 @@ namespace WebApplication4.Controllers
         private Model1 db = new Model1();
 
         // GET: DetailedInformation
-        public ActionResult Index(int? index=0, string orderby="", string orderby1="",string sortby = "")
+        public ActionResult Index(int? index = 0, int yearid = 0, int semesterid = 0, int courseid = 0, string orderby = "", string orderby1 = "", string sortby = "", int flag = 0)
         {
             ViewBag.stdt = db.Students.ToList();
             ViewBag.plans = db.Plans.ToList();
             ViewBag.plansID = index;
             ViewBag.course = db.Course.ToList();
             ViewBag.OrderBy = orderby;
-            if(orderby != orderby1)
+            if (orderby != orderby1)
             {
                 sortby = "";
             }
-            ViewBag.SortBy = sortby == ""?"desc":"";
+            ViewBag.SortBy = sortby == "" ? "desc" : "";
             ViewBag.plancs = db.PlanCourses.ToList();
             List<StudentInfo> StdInfom = GetStudents();
             ViewBag.stcs = StdInfom;
-
-
-
+            var courseyears = Years(yearid);
+            var semesters = Semeters(semesterid);
+            var courseslist = Courses(courseid);
+            ViewBag.courseYears = courseyears;
+            ViewBag.semesters = semesters;
+            ViewBag.courses = courseslist;
+            ViewBag.semesterid = semesterid;
+            ViewBag.courseid = courseid;
+            ViewBag.yearid = yearid;
+            ViewBag.flag = flag;
             List<string> PlanCourses = new List<string>();
-
-            if (index == 0 || index == null)
-            {
-                PlanCourses = db.Course.OrderBy(s => s.courseID).Select(s=>s.courseName).ToList();
-            }
-            else
-            {
-                var plancourses = db.PlanCourses.Include(a => a.Course).Where(a => a.planId == index).Select(a => a.Course).OrderBy(s=>s.courseID).ToList();
-                PlanCourses = plancourses.Select(a => a.courseName).ToList();
-            }
-            PlanCourses.Insert(0, "Name");
-            ViewBag.PlanCourse = PlanCourses.ToList();
-            List<Students> thisPlanStudent = new List<Students>();
+            Dictionary<string, string> PlanCoursesAbbr = new Dictionary<string, string>();
             List<CourseModel> CourseList = new List<CourseModel>();
-
-            if (index == 0 || index == null)
+            if (flag == 0)
             {
-                thisPlanStudent = db.Students.ToList();
-                var courses = db.Course.OrderBy(s=>s.courseID).ToList();
-                foreach(var s in thisPlanStudent)
+                if (index == 0 || index == null)
                 {
-                    var myModel = new CourseModel
+                    var courseslists = db.Course.OrderBy(s => s.courseID).ToList();
+                    foreach (var course in courseslists)
                     {
-                        Name = s.uniUserName,
-                        
-                    };
-                    List<StudentCourses> studentCourses = new List<StudentCourses>();
-                    foreach (var n in courses)
-                    {
-                        StudentCourses studentCourses1 = new StudentCourses();
-                        var courseStudents = db.StudentCourses.Where(c => c.courseID == n.courseID && c.Students.studentID == s.studentID).FirstOrDefault();
-                        if (courseStudents != null)
-                        {
-                            studentCourses.Add(courseStudents);
-                        } else
-                        {
-                            studentCourses1.grade = "";
-                            studentCourses1.courseID = n.courseID;
-                            studentCourses1.studentID = s.studentID;
-                            studentCourses.Add(studentCourses1);
-                        }
-
-                                          
+                        PlanCoursesAbbr[course.courseAbbreviation] = course.courseName;
+                        PlanCourses.Add(course.courseAbbreviation);
                     }
-                    myModel.StudentCourses = studentCourses;
-                    CourseList.Add(myModel);
+                }
+                else
+                {
+                    var plancourses = db.PlanCourses.Include(a => a.Course).Where(a => a.planId == index).Select(a => a.Course).OrderBy(s => s.courseID).ToList();
+                    //PlanCourses = plancourses.Select(a => a.courseAbbreviation).ToList();
+                    foreach (var course in plancourses)
+                    {
+                        PlanCoursesAbbr[course.courseAbbreviation] = course.courseName;
+                        PlanCourses.Add(course.courseAbbreviation);
+                    }
+                }
+                PlanCourses.Insert(0, "Name");
+                PlanCoursesAbbr["Name"] = "Name";
+                ViewBag.PlanCourse = PlanCourses.ToList();
+                ViewBag.PlanCourseDict = PlanCoursesAbbr;
+                List<Students> thisPlanStudent = new List<Students>();
+
+
+                if (index == 0 || index == null)
+                {
+                    thisPlanStudent = db.Students.ToList();
+                    var courses = db.Course.OrderBy(s => s.courseID).ToList();
+                    foreach (var s in thisPlanStudent)
+                    {
+                        var myModel = new CourseModel
+                        {
+                            Name = s.uniUserName,
+
+                        };
+                        List<StudentCourses> studentCourses = new List<StudentCourses>();
+                        foreach (var n in courses)
+                        {
+                            StudentCourses studentCourses1 = new StudentCourses();
+                            var courseStudents = db.StudentCourses.Where(c => c.courseID == n.courseID && c.Students.studentID == s.studentID).FirstOrDefault();
+                            if (courseStudents != null)
+                            {
+                                studentCourses.Add(courseStudents);
+                            }
+                            else
+                            {
+                                studentCourses1.grade = "";
+                                studentCourses1.courseID = n.courseID;
+                                studentCourses1.studentID = s.studentID;
+                                studentCourses.Add(studentCourses1);
+                            }
+
+
+                        }
+                        myModel.StudentCourses = studentCourses;
+                        CourseList.Add(myModel);
+
+                    }
 
                 }
+                else
+                {
+                    thisPlanStudent = db.Students.Where(n => n.planId == index).ToList();
+                    foreach (var n in thisPlanStudent)
+                    {
+                        var courseStudents = db.StudentCourses.Where(m => m.studentID == n.studentID).OrderBy(s => s.courseID).ToList();
+
+                        var myModel = new CourseModel
+                        {
+                            Name = n.uniUserName,
+                            StudentCourses = courseStudents
+                        };
+                        CourseList.Add(myModel);
+                    }
+                }
+
+                //thisPlanStudent= thisPlanStudent.OrderBy(s => s.StudentCourses.Where(d=>d.Course.courseName == "" ).Select(d=>d.grade)).ToList();
+
+                //if the sortby is not empty,sort
 
             }
             else
             {
-                thisPlanStudent = db.Students.Where(n => n.planId == index).ToList();
-                foreach (var n in thisPlanStudent)
+                var selectedcourse = courseslist.Where(s => s.Selected == true).FirstOrDefault()?.Text;
+                var abbr = db.Course.Where(s => s.courseName == selectedcourse).FirstOrDefault()?.courseAbbreviation;
+                var selectedyear = int.Parse(courseyears.Where(s => s.Selected == true).FirstOrDefault()?.Text);
+                var selectedsemester = semesters.Where(s => s.Selected == true).FirstOrDefault()?.Text;
+                if (selectedcourse != null && selectedyear != 0 && selectedsemester != null)
                 {
-                    var courseStudents = db.StudentCourses.Where(m => m.studentID == n.studentID).OrderBy(s => s.courseID).ToList();
+                    var studentcourses = db.StudentCourses.Where(s => s.semester == selectedsemester && s.Course.courseName == selectedcourse && s.year == selectedyear).ToList();
+                    PlanCourses.Add(abbr);
+                    PlanCoursesAbbr[abbr] = selectedcourse;
+                    PlanCourses.Insert(0, "Name");
+                    PlanCoursesAbbr["Name"] = "Name";
 
-                    var myModel = new CourseModel
+                    ViewBag.PlanCourse = PlanCourses.ToList();
+                    ViewBag.PlanCourseDict = PlanCoursesAbbr;
+                    foreach (var studentcourse in studentcourses)
                     {
-                        Name = n.uniUserName,
-                        StudentCourses = courseStudents
-                    };
-                    CourseList.Add(myModel);
+                        List<StudentCourses> studentCourses = new List<StudentCourses>();
+                        studentCourses.Add(studentcourse);
+                        var myModel = new CourseModel
+                        {
+                            Name = studentcourse.Students.uniUserName,
+                            StudentCourses = studentCourses
+                        };
+                        CourseList.Add(myModel);
+                    }
+
                 }
+
             }
 
-            //thisPlanStudent= thisPlanStudent.OrderBy(s => s.StudentCourses.Where(d=>d.Course.courseName == "" ).Select(d=>d.grade)).ToList();
-            
-            //if the sortby is not empty,sort
-            if(!string.IsNullOrEmpty(orderby))
+            if (!string.IsNullOrEmpty(orderby))
             {
 
                 //If sortby is empty, it will be in ascending order, and orderby is name
-                if (orderby == "Name" &&  sortby == "")
+                if (orderby == "Name" && sortby == "")
                 {
-                    CourseList = CourseList.OrderBy(s=>s.Name).ToList();
-                } else if (orderby == "Name" && sortby == "desc")
+                    CourseList = CourseList.OrderBy(s => s.Name).ToList();
+                }
+                else if (orderby == "Name" && sortby == "desc")
                 {
                     CourseList = CourseList.OrderByDescending(s => s.Name).ToList();
-                } else
+                }
+                else
                 {
                     var grad = db.StudentCourses.Where(d => d.Course.courseName == orderby && d.Students.uniUserName != "" && d.Students.planId == 0).ToList();
                     if (sortby == "") //Ascending order
                     {
-                        CourseList = CourseList.OrderBy(s => s.StudentCourses.Where(d =>d.Course!=null&& d.Course.courseName == orderby ).FirstOrDefault()).ToList();
-                    } else if(sortby == "desc") //Descending order
+                        CourseList = CourseList.OrderBy(s => s.StudentCourses.Where(d => d.Course != null && d.Course.courseName == orderby).FirstOrDefault()).ToList();
+                    }
+                    else if (sortby == "desc") //Descending order
                     {
                         CourseList = CourseList.OrderByDescending(s => s.StudentCourses.Where(d => d.Course != null && d.Course.courseName == orderby).FirstOrDefault()).ToList();
                     }
                 }
-                
-                       }
+
+            }
 
             ViewBag.CourseList = CourseList.DistinctBy(a => a.Name).ToList();
-            var projectPeopleAllocations = db.ProjectPeopleAllocations.Include(p => p.Projects);
 
-            return View(projectPeopleAllocations.ToList());
+            return View();
         }
 
         private List<StudentInfo> GetStudents()
@@ -150,7 +209,70 @@ namespace WebApplication4.Controllers
             return StdInfom;
         }
 
+        public List<SelectListItem> Years(int index = 0)
+        {
 
+
+            var courseYears = db.StudentCourses.OrderBy(s => s.year).Select(s => s.year).Distinct().ToList();
+            List<SelectListItem> selectListItems = new List<SelectListItem>();
+            int count = 0;
+            foreach (var course in courseYears)
+            {
+                if (index == count)
+                {
+                    selectListItems.Add(new SelectListItem { Text = course.ToString(), Value = (count++).ToString(), Selected = true });
+
+                }
+                else
+                {
+                    selectListItems.Add(new SelectListItem { Text = course.ToString(), Value = (count++).ToString(), Selected = false });
+
+                }
+            }
+            return selectListItems;
+        }
+        public List<SelectListItem> Semeters(int index = 0)
+        {
+            var semesters = db.StudentCourses.OrderBy(s => s.semester).Select(s => s.semester).Distinct().ToList();
+            List<SelectListItem> selectListItems = new List<SelectListItem>();
+            int count = 0;
+            foreach (var sem in semesters)
+            {
+                if (index == count)
+                {
+                    selectListItems.Add(new SelectListItem { Text = sem.ToString(), Value = (count++).ToString(), Selected = true });
+
+                }
+                else
+                {
+                    selectListItems.Add(new SelectListItem { Text = sem.ToString(), Value = (count++).ToString(), Selected = false });
+
+                }
+            }
+            return selectListItems;
+        }
+        // dropdown box for choose courses
+        public List<SelectListItem> Courses(int index = 0)
+        {
+            var courses = db.Course.OrderBy(s => s.courseName).Select(s => s.courseName).ToList();
+            List<SelectListItem> selectListItems = new List<SelectListItem>();
+
+            int count = 0;
+            foreach (var course in courses)
+            {
+                if (index == count)
+                {
+                    selectListItems.Add(new SelectListItem { Text = course.ToString(), Value = (count++).ToString(), Selected = true });
+
+                }
+                else
+                {
+                    selectListItems.Add(new SelectListItem { Text = course.ToString(), Value = (count++).ToString(), Selected = false });
+
+                }
+            }
+            return selectListItems;
+        }
 
     }
 
